f76a7cada30ffc7e06cc739e60bbea4de6337b35
Timi <timi.rautamaki@gmail.com>
2021-03-04 10:01:41 +0100

    aosp: remove customizations
     * Let's keep this pure

--- config/aosp_device.mk
+++ config/aosp_device.mk
@@ -2,11 +2,6 @@
 PRODUCT_PACKAGES += \
     libril-wrapper
 
-# Extra Apps
-PRODUCT_PACKAGES += \
-    SettingsExtra \
-    LedManagerExtra
-
 # Theme picker
 PRODUCT_PACKAGES += \
     ThemePicker \
@@ -19,21 +14,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     PackagesRemover
 
-# Tri-state-key
-PRODUCT_PACKAGES += \
-    tri-state-key_daemon
-
-# Custom light HAL
-PRODUCT_PACKAGES += \
-    light_daemon
-
 PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 
 PRODUCT_VENDOR_KERNEL_HEADERS := device/oneplus/cheeseburger/kernel-headers
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/data-ipa-cfg-mgr
-
-# Camera API1 ZSL
-PRODUCT_PROPERTY_OVERRIDES += \
-    camera.disable_zsl_mode=1
